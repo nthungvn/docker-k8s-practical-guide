e1ca00a452faf5e59f6517f5f51a5c22343e737e
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-07 22:21:38 +0700

    use bind mount

--- command.sh
+++ command.sh
@@ -2,6 +2,10 @@
 
 docker build -t feedback-node:volumes .
 
-docker run -d -p 80:80 --rm --name feedback-app -v feedback:/app/feedback -v /app/temp feedback-node:volumes
+docker run -d -p 80:80 --rm \
+    --name feedback-app \
+    -v feedback:/app/feedback \
+    -v /Users/nthung/Repos/docker-k8s-practical-guide:/app \
+    -v /app/temp feedback-node:volumes
 
 docker stop feedback-app
