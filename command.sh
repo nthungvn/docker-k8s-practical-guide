e8ead34bcc9daa50d8534154b07b9db73fa5d08c
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-07 23:11:17 +0700

    use EVN as a paramter mapping

--- command.sh
+++ command.sh
@@ -2,12 +2,13 @@
 
 docker build -t feedback-node:volumes .
 
-docker run -d -p 80:80 --rm \
+docker run -d -p 8080:8081 --rm \
     --name feedback-app \
     -v feedback:/app/feedback \
     -v /Users/nthung/Repos/docker-k8s-practical-guide:/app:ro \
     -v /app/temp \
     -v /app/node_modules \
+    -e PORT=8081 \
     feedback-node:volumes
 
 docker stop feedback-app
