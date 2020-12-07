dd506cfbff63f3ce60bdbcc9aa02663d2e1efc18
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-07 22:38:40 +0700

    make the bind mount read only

--- command.sh
+++ command.sh
@@ -5,7 +5,7 @@ docker build -t feedback-node:volumes .
 docker run -d -p 80:80 --rm \
     --name feedback-app \
     -v feedback:/app/feedback \
-    -v /Users/nthung/Repos/docker-k8s-practical-guide:/app \
+    -v /Users/nthung/Repos/docker-k8s-practical-guide:/app:ro \
     -v /app/temp \
     -v /app/node_modules \
     feedback-node:volumes
