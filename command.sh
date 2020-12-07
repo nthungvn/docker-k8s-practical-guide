02841630278e1f95ba5b05568c9fef8ab3efc770
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-07 22:15:02 +0700

    use name volumes

--- command.sh
+++ command.sh
@@ -2,6 +2,6 @@
 
 docker build -t feedback-node:volumes .
 
-docker run -d -p 80:80 --rm --name feedback-app -v /app/feedback feedback-node:volumes
+docker run -d -p 80:80 --rm --name feedback-app -v feedback:/app/feedback -v /app/temp feedback-node:volumes
 
 docker stop feedback-app
