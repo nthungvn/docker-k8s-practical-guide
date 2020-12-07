f32a00c4e018aeb3aab6153c8deac31a36625b8d
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-07 22:27:20 +0700

    anonymous volume for override the mount bind

--- command.sh
+++ command.sh
@@ -6,6 +6,8 @@ docker run -d -p 80:80 --rm \
     --name feedback-app \
     -v feedback:/app/feedback \
     -v /Users/nthung/Repos/docker-k8s-practical-guide:/app \
-    -v /app/temp feedback-node:volumes
+    -v /app/temp \
+    -v /app/node_modules \
+    feedback-node:volumes
 
 docker stop feedback-app
