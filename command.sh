f963f92575a7d6c32ac289e77aacaa54eaf33d7c
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-11 23:49:34 +0700

    connect container via docker network

--- command.sh
+++ command.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+docker network create feedback-network
+
 docker build --tag feedback-node:network .
 
-docker run -d --rm -p 3000:3000 --name feedback-app feedback-node:network
+docker run -d --rm --name mongodb --network feedback-network mongo
 
-docker run -d --rm --name mongodb mongo
+docker run -d --rm -p 3000:3000 --name feedback-app --network feedback-network feedback-node:network
 
 docker logs feedback-app
 docker stop feedback-app
