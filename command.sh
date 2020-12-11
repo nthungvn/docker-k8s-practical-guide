a7411cf419a473273a22405db937f40781818185
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-11 23:41:40 +0700

    connect containers by IP address

--- command.sh
+++ command.sh
@@ -2,7 +2,10 @@
 
 docker build --tag feedback-node:network .
 
-docker run -d --rm -p 80:3000 --name feedback-app feedback-node:network
+docker run -d --rm -p 3000:3000 --name feedback-app feedback-node:network
+
+docker run -d --rm --name mongodb mongo
 
 docker logs feedback-app
 docker stop feedback-app
+docker stop mongodb
