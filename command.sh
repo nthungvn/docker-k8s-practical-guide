e368ef2c286c2117fe3ac5a473c773f983cb3b71
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-12 07:22:47 +0700

    start node service

--- command.sh
+++ command.sh
@@ -11,3 +11,17 @@ docker run -d --rm \
     mongo
 
 docker stop mongodb
+
+
+docker build -t ms-backend:latest .
+
+docker run -d --rm \
+    -p 80:80 \
+    --name ms-backend-app \
+    --network multiple-services \
+    -v ms-backend-log:/app/logs \
+    ms-backend:latest
+
+docker rm ms-backend-app
+docker logs ms-backend-app
+docker stop ms-backend-app
