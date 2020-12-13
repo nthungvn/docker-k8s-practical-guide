74f7180c3be8047a0cbb0f82f4ead3a73c9c877a
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-13 16:44:40 +0700

    create the utility container for create a node project

--- command.sh
+++ command.sh
@@ -4,9 +4,24 @@
 docker run -it node:14
 
 #2
-docker run -it -d --rm --name node-ultil node:14
-docker exec -it node-ultil npm init
-docker stop node-ultil
+docker run -it -d --rm --name node-util node:14
+docker exec -it node-util npm init
+docker stop node-util
 
 #3
-docker run -it --rm --name node-ultil node:14 npm init
+docker run -it --rm --name node-util node:14 npm init
+
+#--------------------------------------------
+
+#4
+docker build -t node-util:latest .
+docker run -it -d --rm \
+  -v /Users/nthung/Repos/docker-k8s-practical-guide:/app \
+  -v /app/node_modules \
+  --name node-util \
+  node-util:latest
+
+docker exec -it node-util npm init
+docker exec -it node-util npm install -s express
+
+docker stop node-util
