890aace7905ba6ea75b2ad06da0a5bc20b14c1b1
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-13 20:48:09 +0700

    start services and app

--- command.sh
+++ command.sh
@@ -2,3 +2,6 @@
 
 docker-compose --build
 docker-compose run --rm composer create-project --prefer-dist laravel/laravel .
+
+# It will start the dependencies container
+docker-compose up -d --build server
