c67e6de4097903aeda20c84e5f08ec6691048924
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-13 21:04:49 +0700

    add more utility container

--- command.sh
+++ command.sh
@@ -5,3 +5,5 @@ docker-compose run --rm composer create-project --prefer-dist laravel/laravel .
 
 # It will start the dependencies container
 docker-compose up -d --build server
+
+docker-compose run --rm artisan migrate
