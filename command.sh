b44beeeefb883400ea52b75f9331d9f0707d230d
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-19 22:34:30 +0700

    rollback and history

--- command.sh
+++ command.sh
@@ -16,11 +16,22 @@ kubectl scale deployment/k8s-first-deployment --replicas=3
 kubectl get pods
 kubectl scale deployment/k8s-first-deployment --replicas=1
 
+# Update the deployment: Failed
 docker build -t sunshineaact/k8s-first-app .
 docker push sunshineaact/k8s-first-app
 kubectl set image deployment/k8s-first-deployment k8s-first-app=sunshineaact/k8s-first-app
 kubectl get deployment
 
+# Update the deployment: OK
 docker build -t sunshineaact/k8s-first-app:1 .
 docker push sunshineaact/k8s-first-app:1
 kubectl set image deployment/k8s-first-deployment k8s-first-app=sunshineaact/k8s-first-app:1
+
+# Rollback
+kubectl set image deployment/k8s-first-deployment k8s-first-app=sunshineaact/k8s-first-app:2
+kubectl rollout undo deployment/k8s-first-deployment
+kubectl get pods
+
+kubectl rollout history deployment/k8s-first-deployment
+kubectl rollout history deployment/k8s-first-deployment --revision=3
+kubectl rollout undo deployment/k8s-first-deployment --to-revision=1
