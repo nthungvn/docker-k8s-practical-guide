20dcc5bcb5268c10d6b81faa699c2b8c3ed13ba5
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-19 22:15:09 +0700

    create a deployment object

--- command.sh
+++ command.sh
@@ -2,3 +2,5 @@
 
 docker build -t sunshineaact/k8s-first-app .
 docker push sunshineaact/k8s-first-app
+kubectl create deployment k8s-first-deployment --image=sunshineaact/k8s-first-app
+kubectl get deployment
