cc9a6da945bb5e55329751887655dd6b785c4161
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-19 22:15:26 +0700

    expose/create the service

--- command.sh
+++ command.sh
@@ -4,3 +4,7 @@ docker build -t sunshineaact/k8s-first-app .
 docker push sunshineaact/k8s-first-app
 kubectl create deployment k8s-first-deployment --image=sunshineaact/k8s-first-app
 kubectl get deployment
+
+kubectl expose deployment k8s-first-deployment --type=LoadBalancer --port=8080
+
+minikube service k8s-first-deployment
