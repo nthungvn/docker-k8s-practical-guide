f0c1a6e5a8aaa82125a048e1c5daee9493cc5519
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-21 06:41:37 +0700

    add emptyDir volume

--- command.sh
+++ command.sh
@@ -4,3 +4,7 @@ docker build -t sunshineaact/k8s-third-app .
 docker push sunshineaact/k8s-third-app
 
 kubectl apply -f service.yaml -f deployment.yaml
+
+# Create a emptyDir volume
+kubectl apply -f deployment.yaml
+minikube service k8s-third-service
