4f67badd491e497268bc146925a8fbc472f940aa
[Miracle] Hung <hung.nguyenthai@axonactive.com>
2020-12-21 12:44:51 +0700

    set env from deployment file

--- command.sh
+++ command.sh
@@ -13,3 +13,10 @@ minikube service k8s-third-service
 kubectl apply -f host-pv.yaml
 kubectl apply -f host-pvc.yaml
 kubectl apply -f deployment.yaml
+
+# ENV
+
+docker build -t sunshineaact/k8s-third-app .
+docker tag sunshineaact/k8s-third-app sunshineaact/k8s-third-app:3
+docker push sunshineaact/k8s-third-app:3
+kubectl apply -f deployment.yaml
