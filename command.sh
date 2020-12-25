ac83d93916533c2e27f2942e26b88fbe0a00fdc9
Hung Nguyen <nthung.vlvn@gmail.com>
2020-12-25 21:50:03 +0700

    Add volume for users folder writing logs

--- command.sh
+++ command.sh
@@ -8,4 +8,9 @@ docker push sunshineaact/kub-dep-users:latest
 docker tag docker-k8s-practical-guide_auth sunshineaact/kub-dep-auth:latest
 docker push sunshineaact/kub-dep-auth:latest
 
-kubectl apply -f auth.yaml -f users.yaml
+docker tag docker-k8s-practical-guide_tasks sunshineaact/kub-dep-tasks:latest
+docker push sunshineaact/kub-dep-tasks:latest
+
+kubectl apply -f mongo.yaml
+kubectl apply -f users-pv.yaml
+kubectl apply -f auth.yaml -f users.yaml -f tasks.yaml
